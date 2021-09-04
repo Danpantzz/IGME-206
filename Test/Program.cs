f96f197ae20c1b7b2f731191c0ebca316d91b25b
Daniel McErlean <djm7966@ad.rit.edu>
2021-09-03 23:40:37 -0400

    Homework question 5

---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkProblem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 4 numbers:");
+
+            int num1 = Convert.ToInt32(Console.Read());
+            int num2 = Convert.ToInt32(Console.Read());
+            int num3 = Convert.ToInt32(Console.Read());
+            int num4 = Convert.ToInt32(Console.Read());
+
+            Console.WriteLine(num1 + " " + num2 + " " + num3 + " " + num4);
+
+        }
+    }
+}
