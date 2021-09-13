9875374a6f09a9639f8f27cc6047870e2b0a4c87
Daniel McErlean <djm7966@ad.rit.edu>
2021-09-13 19:56:10 -0400

    Homework 8, number 9

---
+++ PE8 - Number 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8___Number_9
+{
+    //Class: Program
+    //Author: Daniel McErlean
+    //Purpose: place double quotes around every word in a string
+    //Restrictions: None
+    class Program
+    {
+        //Method: Main
+        //Purpose: Prints out every word in a string with double quotes around it
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            string newWord;
+            string userInput;
+
+            Console.Write("Enter a string: ");
+            userInput = Console.ReadLine();
+
+            string[] words = userInput.Split(' ');
+
+            foreach (string word in words)
+            {
+                newWord = "\"" + word + "\"";
+                Console.WriteLine(newWord);
+            }
+
+        }
+    }
+}
