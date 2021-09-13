5d67c997bfb2fae6d66947a920f584b7f09db806
Daniel McErlean <djm7966@ad.rit.edu>
2021-09-13 19:30:47 -0400

    Homework 8, number 8

---
+++ PE8 - Number 8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8___Number_8
+{
+    //Class: Program
+    //Author: Daniel McErlean
+    //Purpose: replace "no" with "yes"
+    //Restrictions: None
+    class Program
+    {
+        //Method: Main
+        //Purpose: Takes a string to replace al words "no" with "yes"
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            string userInput;
+            string replaced = "";
+            string newWord = "yes";
+
+            Console.Write("Enter a string: ");
+            userInput = Console.ReadLine();
+
+            if (userInput.ToLower().Contains("no"))
+            {
+                replaced = userInput.ToLower().Replace("no", "yes");
+            }
+            if (userInput.ToLower().Contains("no,"))
+            {
+                replaced = userInput.ToLower().Replace("no,", "yes,");
+            }
+
+            Console.WriteLine(replaced);
+
+            /*
+            string[] words = userInput.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (word.ToLower().Contains("no"))
+                {
+                    replaced += newWord;
+                    continue;
+                }
+                replaced += word;
+            }
+
+            Console.WriteLine(replaced);
+            */
+
+        }
+    }
+}
