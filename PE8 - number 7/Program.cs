810f13d9b99fd90daecfd3cd461b4cd838a1bf79
Daniel McErlean <djm7966@ad.rit.edu>
2021-09-13 18:25:46 -0400

    Homework 8, number 7

---
+++ PE8 - number 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8___number_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string reverse = "";
+            int i;
+            int length;
+
+
+            Console.Write("Enter a string: ");
+            userInput = Console.ReadLine();
+            length = userInput.Length;
+
+            for (i = 0; i < length; i++)
+            {
+                reverse += userInput.ElementAt((length - 1) - i);
+            }
+
+            Console.WriteLine("Your string, reversed: {0}", reverse);
+
+        }
+    }
+}
