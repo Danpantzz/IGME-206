3975d36cb1a63998b073d1c54ac982302e0370b9
Daniel McErlean <djm7966@ad.rit.edu>
2021-09-13 19:33:36 -0400

    Added Comments

--- PE8 - number 7/Program.cs
+++ PE8 - number 7/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace PE8___number_7
 {
+    //Class: Program
+    //Author: Daniel McErlean
+    //Purpose: Reverse a string
+    //Restrictions: None
     class Program
     {
+        //Method: Main
+        //Purpose: Takes any string and prints out the reverse
+        //Restrictions: None
         static void Main(string[] args)
         {
             string userInput;
@@ -15,16 +22,18 @@ namespace PE8___number_7
             int i;
             int length;
 
-
+            //Ask for string input
             Console.Write("Enter a string: ");
             userInput = Console.ReadLine();
             length = userInput.Length;
 
+            //reverses the string by taking the length ( - 1) minus the element i is currently at
             for (i = 0; i < length; i++)
             {
                 reverse += userInput.ElementAt((length - 1) - i);
             }
 
+            //Print reversed string
             Console.WriteLine("Your string, reversed: {0}", reverse);
 
         }
