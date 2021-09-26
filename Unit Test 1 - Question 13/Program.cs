9115d094698f6bff9f4fc384311b09c9c668158e
Daniel McErlean <djm7966@ad.rit.edu>
2021-09-26 17:59:31 -0400

    Created #12 using a struct

---
+++ Unit Test 1 - Question 13/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_1___Question_13
+{
+
+    //Struct: Employee
+    //Purpose: Declare employee variables
+    //Restrictions: None
+    struct employee
+    {
+        public string sName;
+        public double dSalary;
+    }
+
+    //Class: Program
+    //Author: Daniel McErlean
+    //Purpose: declare and raise salary of type employee
+    //Restrictions: None
+    class Program
+    {
+        
+        static employee me = new employee();
+
+        //Method: Main
+        //Purpose: Raise salary of employee me with my name
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            me.sName = Console.ReadLine();
+            me.dSalary = 30000;
+
+            if (GiveRaise(me) == true)
+            {
+                Console.WriteLine("Congratulations on your raise! Your salary is now {0}.", me.dSalary);
+            }
+            
+        }
+
+        //Method: GiveRaise
+        //Purpose: Check to see if name == "Daniel", then give raise
+        //Restrictions: None
+        static bool GiveRaise(employee person)
+        {
+            if (person.sName == "Daniel")
+            {
+                me.dSalary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
