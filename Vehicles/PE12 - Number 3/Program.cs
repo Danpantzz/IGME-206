bbb916187f7e5c5164e34817c1f119ef98a3ce6c
Daniel McErlean <djm7966@ad.rit.edu>
2021-10-01 22:36:06 -0400

    Created derived class

---
+++ Vehicles/PE12 - Number 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12___Number_3
+{
+    public class myClass
+    {
+        private string myString = "String";
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+
+        static void Main(string[] args)
+        {
+            MyDervivedClass n = new MyDervivedClass();
+
+            
+
+            Console.Write(n.GetString());
+        }
+    }
+    
+    public class MyDervivedClass : myClass
+    {
+
+        public override string GetString()
+        {
+            return base.GetString() + " (output from the derived class)\n";
+        }
+    }
+    
+}
