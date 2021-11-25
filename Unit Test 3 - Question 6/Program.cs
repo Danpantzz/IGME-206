bf6680c7ca283000747607679fe729acc837b6ca
Daniel McErlean <djm7966@ad.rit.edu>
2021-11-24 20:56:56 -0500

    Not sure how to do these.

---
+++ Unit Test 3 - Question 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_3___Question_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
